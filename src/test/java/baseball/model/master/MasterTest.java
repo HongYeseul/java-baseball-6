e855edb42992eb30bfb12db192cbcdae92a05a66
HongYeseul <ghd8119@naver.com>
2023-11-24 16:43:09 +0900

    :white_check_mark: test(Master): 게임을 종료할 것인지 판단하는 테스트 추가

--- src/test/java/baseball/model/master/MasterTest.java
+++ src/test/java/baseball/model/master/MasterTest.java
@@ -16,11 +16,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MasterTest {
-    Answer answer;
-    @BeforeEach
-    void init() {
-        answer = new Answer(Arrays.asList(1,2,3));
-    }
 
     @DisplayName("[SUCCESS] 정답과 유저의 값을 비교하여 결과 값을 반환한다.")
     @ParameterizedTest(name = "유저가 입력한 값: {0}, 반환 값(결과값): {1}")
@@ -31,9 +26,18 @@ class MasterTest {
             "839, 1볼"
     })
     void makeResult(String userInput, String output){
+        Answer answer = new Answer(Arrays.asList(1,2,3));
         User user = new User();
         Master master = new Master();
         String result = master.makeResult(answer, user.inputGameNumber(userInput));
         assertThat(result).isEqualTo(output);
+    }
+
+    @DisplayName("[SUCCESS] 게임을 종료할 것인지 판단한다.")
+    @ParameterizedTest(name = "유저가 입력한 값: {0}, 반환 값(결과값): {1}")
+    @CsvSource({"1, true", "2, false"})
+    void isGameRestart(int flag, boolean result) {
+        Master master = new Master();
+        assertThat(master.isGameRestart(flag)).isEqualTo(result);
     }
 }
