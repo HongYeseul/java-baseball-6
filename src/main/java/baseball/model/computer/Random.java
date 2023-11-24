0adfef2924251e141243837fb6cfd6854cb87e9c
HongYeseul <ghd8119@naver.com>
2023-11-24 14:48:29 +0900

    :sparkles: feat(random): 랜덤 값 만드는 기능 추가

--- src/main/java/baseball/model/computer/Random.java
+++ src/main/java/baseball/model/computer/Random.java
@@ -1,4 +1,19 @@
 package baseball.model.computer;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Random {
+    public static Answer makeAnswer() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return new Answer(randomNumbers);
+    }
 }
