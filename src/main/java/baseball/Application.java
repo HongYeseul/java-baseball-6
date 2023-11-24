dd66b82e83946692de1a4cc88e361d6c642c72ab
HongYeseul <ghd8119@naver.com>
2023-11-24 16:45:54 +0900

    :sparkles: feat(main): 프로그램 진입점 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.controller.MainController;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MainController mainController = new MainController(
+                new InputView(),
+                new OutputView()
+        );
+
+        mainController.play();
     }
 }
--- src/main/java/baseball/controller/MainController.java
+++ src/main/java/baseball/controller/MainController.java
@@ -1,4 +1,17 @@
 package baseball.controller;
 
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class MainController {
+    InputView inputView;
+    OutputView outputView;
+
+    public MainController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void play() {
+    }
 }
